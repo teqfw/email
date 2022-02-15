991019aec187680e048759179ccfbb01bd5a1f31
Alex Gusev <alex@flancer64.com>
2022-02-15 23:32:14 +0200

    WF-569 Logs aggregation service

--- src/Back/Process/Email.mjs
+++ src/Back/Process/Email.mjs
@@ -21,7 +21,7 @@ function Factory(spec) {
     /** @type {TeqFw_Email_Back_Defaults} */
     const DEF = spec['TeqFw_Email_Back_Defaults$'];
     /** @type {TeqFw_Core_Shared_Api_ILogger} */
-    const logger = spec['TeqFw_Core_Shared_Api_ILogger$'];
+    const logger = spec['TeqFw_Core_Shared_Api_ILogger$$']; // instance
     /** @type {TeqFw_Core_Back_Config} */
     const config = spec['TeqFw_Core_Back_Config$'];
 
